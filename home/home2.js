55603ace5f885f4617b60d16371957b6961b2a60
s0809j <s0809j@stu.ac.kr>
2025-04-17 22:16:03 +0900

    Update home2.js

--- home/home2.js
+++ home/home2.js
@@ -196,7 +196,7 @@ function closeLoginModal() {
   document.getElementById("loginModal").style.display = "none";
 }
 
-function handleLogin() {
+function goToLogin() {
   const nickname = "닉네임"; // 예시
   document.querySelector(".nickname").textContent = nickname;
 

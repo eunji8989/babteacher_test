fc0579fa39b937d670f95f18c78275ea81a953f4
s0809j <s0809j@stu.ac.kr>
2025-05-20 15:18:34 +0900

    Update main.js

--- test/js/main.js
+++ test/js/main.js
@@ -1,11 +1,12 @@
 // main.js
+import { modalTerms, modalForm } from './modalElements.js';
+import { openSignupModal, openLoginModal } from './modalControls.js';
 import { addEventListeners } from './eventListeners.js';
 import { initTermsAgreement } from './termsAgreement.js';
 import { initCustomPlaceholder } from './customPlaceholder.js';
 import { initEmailDomainSelect } from './emailDomainSelect.js';
 import { setupUserMenuOutsideClick } from './loginUser.js';
 import { initKakaoUser } from './kakaoUserInit.js';
-import { openSignupModal, openLoginModal } from './modalControls.js';
 
 function init() {
   addEventListeners();

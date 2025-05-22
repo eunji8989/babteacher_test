a0dad69a86b56eb6f06ca00cb89278bf162b4b4c
s0809j <s0809j@stu.ac.kr>
2025-05-22 17:30:13 +0900

    Delete test/js directory

--- test/js/modalElements.js
+++
@@ -1,6 +0,0 @@
-// modalElements.js
-export const modalTerms = document.getElementById("modal-terms");
-export const modalForm = document.getElementById("modal-form");
-export const modalComplete = document.getElementById("modal-complete");
-export const modalLogin = document.getElementById("modal-login");
-export const modalFindPw = document.getElementById("modal-findpw");
